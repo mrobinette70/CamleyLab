c2f19b2774aca7cbf8f382e8b29c6376bb73b4b4
Michael Robinette <mrobine2@Jhu.edu>
2019-09-04 14:31:59 -0400

    The functional backbone of the project. Left to do is the actual parameter tuning of real cells (as in Lin et al)

--- CellPlayground.py
+++ CellPlayground.py
@@ -4,20 +4,105 @@ Created on Fri Jul  5 15:06:54 2019
 
 @author: micha
 """
+"""
+Questions:
+    Should inh be wiped after contact ends? Or should the inhibitor be allowed to 
+    keep diffusing?
+    
+
+"""
+
 
 import numpy as np
 import matplotlib.pyplot as plt
-from CellClass import CellGuy
+from CellTest import CellTest
 
 
-env_length = 150
+env_length = 50000
 cell_env = np.zeros(env_length,float)
-chem = np.zeros(env_length,float)
-c1 = CellGuy(0,.1,.05,.05)
-c2 = CellGuy(1,.1,.05,.05)
+#env_chem = np.zeros(env_length,float)#
+env_chem = np.linspace(0,100,env_length)
+c1 = CellTest(start_loc=7500,beta=.1,j=1,omega=.9)
+c2 = CellTest(start_loc=500, beta=.1,j=1,omega=1)
+c3 = CellTest(start_loc=12000,beta=.1,j=1,omega=0.01)
+c4 = CellTest(start_loc=10000, beta=.1,j=1,omega=0.1)
 
+c2.Rhos=np.zeros(c2.N+1)
+c2.Rhos[81:c2.N+1] = 0.2
 #plt.plot(c1.rhos,'.')
 
+def boundary_check(cells, env_length):
+    '''
+    For each cell, checking if in contact with the other cells. If so, the cell 
+    is in "contact"
+    
+    cells: a tuple of all cells (from instance of CellClass)
+    env_
+    
+    Start with the first cell and push other cells out
+    
+    Label by cell #, instead of all 1. Count all 2s in cell 1 and push cell 2 out by that length
+    This doesn't account for cells being bordered by two cells
+    '''
+    cell_env = np.zeros(env_length)
+    for idx,c in enumerate(cells):
+        cell_env[c.position:c.end+1] = idx+1 #establishing all cell positions
+    for c in cells:
+        
+        if cell_env[c.position-1] != 0: #there's a cell to the left
+            cellnum = cell_env[c.end+1] #number of the other cell
+            howmany = np.count_nonzero(cell_env[c.position:c.end+1] == cellnum)
+        
+            c.position += howmany
+            c.end += howmany
+            c.left_contact = True
+        else:
+            c.left_contact = False
+            
+        if cell_env[c.end+1] != 0: #there's a cell to the right
+            cellnum = cell_env[c.end+1] #number of the other cell
+            howmany = np.count_nonzero(cell_env[c.position:c.end+1] == cellnum)
+            c.position -= howmany
+            c.end -= howmany
+            c.right_contact = True
+        else:
+            c.right_contact = False
+        
+def place_cells(cells, env_length):
+    '''
+    This function takes all cell Rho profiles and collects them into a single array
+    for plotting over time.
+
+    cells: a tuple of all cells (from instance of CellClass)
+    env_length: the length of the 1D environment cells swim in    
+    '''
+    cell_env = np.zeros(env_length,float)
+    #if len(cells) > 1:
+    for c in cells:  
+        cell_env[c.position:c.end+1] = c.Rhos
+    return cell_env
+
+def build_timegraph(h, t, env_length, every=1):
+    '''
+    Builds the array used to show cells over time in a surface/heat map
+    (time axis is axis 0)
+    
+    Parameters:
+        
+        h: float
+            Timestep used in CellTest.diffuse(). Retrieve from an instance of 
+            CellTest.
+        t: float, int
+            Time that will elapse over the period of the simulation
+        env_length: float, int
+            Length of the environment cells can swim in
+        every: int
+            How frequently the cell will be plotted. every=1 plots the cell at 
+            every timestep.
+    '''
+    time_array = np.zeros([int(np.ceil(t/(h*every)))+1,env_length])
+    return time_array
+"""
 def vis_cells(cells,env_length):
     '''
     This function visualizes all cells in a given environment.
@@ -25,33 +110,60 @@ def vis_cells(cells,env_length):
     cells: a tuple of all cells (from instance of CellClass)
     env_length: the length of the 1D environment cells swim in
     '''
-    cell_env = np.zeros(env_length,float)
-    #if len(cells) > 1:
-     #   for cell in cells:  
-      #      cell_env[cell.position:cell.end] = cell.rhos
-    #else:
-    cell_env[cells.position:cells.end] = cells.rhos
+
     plt.plot(cell_env,'.r')
-    
+"""    
 #vis_cells((c1),150)
         
 
 t = 0
 t_end = 100
-cells = [c1]
+cells = [c1,c2,c3,c4]
 #for cell in cells:
-    
-while t < t_end:
-        #for cell in cells:
-    cell.diffuse(chem[cell.position:cell.end])
-    #c1.diffuse(chem)#[c1.position:c1.end])
-    
-    t += c1.h
-        #print(cell.rho_a)# + cell.rho_b)
-vis_cells((c1),100)
-print(c1.rho_a+c1.rho_b)
-        
 
+#plt.plot(c1.Rhos)
+
+print('Start and end: ',c1.position,', ',c1.end)
+print(' ')
+
+counter = 0
+tg = build_timegraph(c1.h, t_end, env_length, every=1)
+
+while t < t_end:
+    for c in cells:
+    #cell.diffuse(chem[cell.position:cell.end])
+        c.diffuse(env_chem)
+        c.move()
+        '''
+        if c1.left_contact == True:
+            plt.figure(1)
+            plt.plot(c1.Rhos)
+            plt.figure(2)
+            plt.plot(c1.inh)
+            print(c1.vel)
+        if c2.right_contact == True:
+            print(t)
+        '''    
+    #boundary_check([cell_list])
+    t += c1.h
+    boundary_check(cells,env_length)
+    allcells = place_cells(cells, env_length)
+
+    ''' filling array to have cells over time '''
+    ###fill array at [counter,:]
+    tg[counter,:] = allcells
+    counter += 1
+    
+
+    #print('Velocity: ',c1.vel)
+    #print('Start and end: ',c1.position,', ',c1.end)
+    #print(' ')
+        #print(cell.rho_a)# + cell.rho_b)
+#plt.figure(2)
+#vis_cells((c1),env_length)
+#print(c1.rho_a+c1.rho_b)
+#plt.figure(3)        
+#plt.plot(c1.Rhos)
 '''
 how can I include Rhos in the plot?
 
--- CellTest.py
+++ CellTest.py
@@ -33,28 +33,51 @@ class CellTest:
     
     #def rho_CI(Rhos_in,beta=0.05,j=0.05,Di=0.1,omega=0.7,chem_max=0.5): #all of the parameters I'd like to test
     def __init__(self, start_loc, beta=0.05, 
-                 j=0.05, Di=0.1, omega=0.7, 
-                 chem_max=0.5): #customizable inputs
+                 j=0.05, Di=0.1, omega=0.7): #customizable inputs
         
         self.beta = beta
-        self.j = j
+        self.j = 0
+        self.j_contact = j #assuming the cell is not in contact, then if in contact this will be the influx of inh
         self.Di = Di
         self.omega = omega
-        self.chem_max = 0.5 #this should be in the cell's environment 
+        
         self.position = start_loc
-        
-        self.end = start_loc + self.L
-        
-        #self.Rhos = np.random.random(size=self.N+1)
-        self.Rhos=np.zeros((self.N+1),float)
-        self.Rhos[81:self.N+1]=0.2
+        self.end = self.position + self.N
+       
+        self.Rhos = np.random.random(size=self.N+1)*.3
+        #self.Rhos=np.zeros((self.N+1),float)
+        #self.Rhos[81:self.N+1]=0.2
+        #self.Rhos[0:20] = 0.2
         self.Rhos2 = np.zeros((self.N+1),float)
         self.inh=np.zeros((self.N+1),float)
         self.inh2=np.zeros((self.N+1),float)
-        self.contact = False #False, 'left', or 'right'
+        #self.contact = False #False, 'left', or 'right' or 'both'
+        self.left_contact = False
+        self.right_contact = False
+        'maybe instead this should be left_contact or right_contact instead of contact'
 
     
     def react_CI(self, a, b, inh, chem):
+        '''
+        Reaction part of RDE - wave-pinning model from Mori et. al, balancing
+        a positively-cooperative onrate and concentration-dependent linear offrate.
+        Used in diffuse()
+        
+        Parameters:
+            
+        a: array-like
+            Profile of active (membrane-bound) Rho proteins within the cell
+        b: float
+            Profile of inactive (cytosolic) Rho proteins within the cell - assumed
+            to be constant in this approximation, since cytosolic proteins diffuse 
+            several orders of magnitude faster than membrane-bound varieties.
+        inh: array-like
+            Profile of inhibitor molecules that cause contact-inhibition of locomotion
+            (CIL). 
+        chem: array-like
+            Chemoattractant profile of the environment. The RDE uses the chemoattractant within the 
+            length of the cell to determine chemotaxis
+        '''    
         onrate=b*(self.k0+self.gamma*a**2/(self.K**2+a**2)
                 - self.beta*inh + self.omega*chem)
         onrate[onrate<0]=0
@@ -62,70 +85,122 @@ class CellTest:
         #this is the RDE, including an inhibitor that propels Rho away and a chemoattractant
         return (onrate - offrate)
     
-    def diffuse(self,tEnd):
-        # Diffusing forward one iteration
+    def diffuse(self,env_chem):
+        ''' 
+        Diffusing forward one iteration
+        Parameters:
+            
+        env_chem: array-like
+            Chemoattractant profile of the environment. The RDE uses the chemoattractant within the 
+            length of the cell (chem) to determine chemotaxis
+            
+        To add:
+            what happens at contact? It could just be
+            self.jcontact = 0.05     in __init__(),
+            self.j = 0 (no contact)
+            if contact != False:
+                j=self.jcontact
+            This will make sure j can be customized, but it will start as 0 until
+            contacting another cell (the details about 'left' and 'right' can be figured out later)
+                
+        '''
         
         N = self.N #having this call self.N gets very messy and hard to read
         Rhos = self.Rhos
         Rhos2 = self.Rhos2
         inh = self.inh
         inh2 = self.inh2
-        """introduction of inhibitor and chemoattractant"""
-        #Rhos2=np.zeros((N+1),float)
-        #inh=np.zeros((N+1),float)
-        #inh2=np.zeros((N+1),float)
-        chem=np.zeros((N+1),float)
         
-        #chem=np.linspace(chem_max,0,101) 
-        ####chem=np.linspace(0,self.chem_max,101)
+        
+        chem=env_chem[self.position:self.end+1]
+        
+        if self.left_contact == True or self.right_contact == True:
+            self.j = self.j_contact
+        else:
+            self.j = 0 #checking if the cells detach - the influx will need to return to 0
+
        
         Rhos2[0]=Rhos[0]; Rhos2[N]=Rhos[N]
-        inh[N]=self.j #some arbitrary concentration to start with
-        inh2[0]=0; inh2[N]=inh[N]
-        #t=0
-        #tEnd2=200 #change this to 200
-    
-#        while t<tEnd: # this needs to be pulled out - the while loop will be called in the environment
-#            Rhos[0] = (Rhos[2] + Rhos[1])/2    
-#            Rhos[N] = (Rhos[N-2] + Rhos[N-1])/2
-#            #Rhos[Rhos<0]=0 #avoids negative values -- all active Rho (a) is gone
-#            inh[0] = (inh[2] + inh[1])/2
-#            inh[N] = self.j/self.Di + inh[N-1]
-#            
-#            a=np.sum(self.Rhos)/self.L     
-#            b=(self.Ntot-a*self.L)/self.L
-#            
-#            #diffusion and RD for inhibitor and Rho
-#            inh2[1:N] = inh[1:N] + self.const*(inh[2:N+1]+inh[0:N-1]-2*inh[1:N])
-#            Rhos2[1:N] = Rhos[1:N] + self.const*(Rhos[2:N+1]+Rhos[0:N-1]-2*Rhos[1:N]) + self.h*self.react_CI(Rhos[1:N],b,inh[1:N],chem[1:N])
-#            #Rhos2[Rhos2<0]=0
-#            Rhos,Rhos2 = Rhos2,Rhos
-#            inh,inh2 = inh2,inh
-#            t += self.h
+
+
+        if self.right_contact == True & self.left_contact == True:
+            inh[N]=self.j 
+            inh2[0]=inh[0]; inh2[N]=inh[N]
+            
+            inh[0] = self.j/self.Di + inh[1] #check the sign here
+            inh[N] = self.j/self.Di + inh[N-1]
+        elif self.left_contact == True:
+            inh[0]=self.j 
+            inh2[0]=inh[0]; inh2[N]=inh[N]            
+            
+            inh[0] = self.j/self.Di + inh[1]
+            inh[N] = inh[N-1]
+        elif self.right_contact == True:
+            inh[N]=self.j 
+            inh2[0]=inh[0]; inh2[N]=inh[N]
+            
+            inh[0] = (inh[2] + inh[1])/2
+            inh[N] = self.j/self.Di + inh[N-1]
+        else:
+            inh[0] = 0
+            inh[N] = 0
+            
+        Rhos[0]=(Rhos[2]+Rhos[1])/2
         
-        #Rhos[0] = (Rhos[2] + Rhos[1])/2    
-        #Rhos[N] = (Rhos[N-2] + Rhos[N-1])/2
-      
-        #Rhos[0] = Rhos[1]
-        #Rhos[N] = Rhos[N-1]
-        
-        inh[0] = (inh[2] + inh[1])/2
-        inh[N] = self.j/self.Di + inh[N-1]
-        Rhos[0]=(Rhos[2]+Rhos[1])/2  
         Rhos[N]=(Rhos[N-2]+Rhos[N-1])/2    
         a=np.sum(Rhos)/self.L     
         b=(self.Ntot-a*self.L)/self.L
-            
+
         #diffusion and RD for inhibitor and Rho
         inh2[1:N] = inh[1:N] + self.const*(inh[2:N+1]+inh[0:N-1]-2*inh[1:N])
-        Rhos2[1:N] = Rhos[1:N] + self.const*(Rhos[2:N+1]+Rhos[0:N-1]-2*Rhos[1:N]) + self.h*self.react_CI(Rhos[1:N],b,inh[1:N],chem[1:N])
+        #print('Chem len: ',len(chem[self.position:self.end-1]))
+        Rhos2[1:N] = Rhos[1:N] + self.const*(Rhos[2:N+1]+Rhos[0:N-1]-2*Rhos[1:N]) + self.h*self.react_CI(Rhos[1:N],b,inh[1:N],chem[1:N])#[self.position:self.end-1])
  
         #Rhos,Rhos2 = Rhos2,Rhos
-        #inh,inh2 = inh2,inh        
+        #inh,inh2 = inh2,inh
         #Rhos[0] = Rhos[1]
         #Rhos[N] = Rhos[N-1]
-        
 
-        
+
+
         self.Rhos = Rhos2 #equivalent to a return     #, chem, inh
         self.inh = inh2   #profile of inhibitor
+        
+    def move(self):
+        '''
+        This function calculates the velocity of a cell given its Rho profile.
+        Since the cell is at low Reynolds number, assume no momentum is kept over
+        timesteps.
+        
+        Problems:
+            What if it reaches a wall? This throws an error now.
+            Should a cell's velocity depend on a contacting cell? Maybe the velocity for 
+            both cells will be the sum of both velocities until contact breaks.
+            ^^ Should vel be calculated somewhere else then? Either as another method or 
+            as a function in the environment that accesses rho?
+        '''
+        self.vel = 100*(self.Rhos[self.N] - self.Rhos[0])
+        self.roundvel = int(np.rint(self.vel))
+        
+        #vel > 0: move right
+        if self.vel > 0 and self.right_contact == True: #unless contacting another cell!
+            self.vel = 0
+        elif self.vel < 0 and self.left_contact == True: #same with moving left
+            self.vel = 0
+        else:
+            self.position += self.roundvel
+            self.end += self.roundvel
+            
+    #def update(self):
+     #   if                     
+'''
+t=0
+
+
+while t < 100:
+
+    c.diffuse(100)
+    if int(t) % 20 == 0:
+        plt.plot(c.Rhos)
+    t += c.h
+'''
